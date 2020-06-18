2949e3a4b4290f9a4cd4fe53c5e1648953c4e090
ordo <ordo@hyrule.ndc.nasa.gov>
2020-06-18 08:37:54 -0700

    Need to install different OS, mongo on void is bad

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
+// Setting up the Basic Express Server Backend
+// Start server with `nodemon server` 
+// Listens on port 4000
+
+// Begin const declaration
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;app.use(cors());
+// End const declaration
 
+// Begin Logging
 app.use(bodyParser.json());app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
+// End Logging
