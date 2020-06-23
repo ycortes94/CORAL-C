94588b5b3bd28d925fc7d133a5749e6056b16608
Ordo-S <matt@spadarolabs.com>
2020-06-23 12:01:29 -0700

    Adding in basic DB connectino and setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,33 @@
 // Setting up the Basic Express Server Backend
 // Start server with `nodemon server` 
 // Listens on port 4000
+// Start mongo with `mongod`
+// create db by launching mongo then type `use biz`
+// default port is 27017, if you change this change 
+// mongoose.connect
 
 // Begin const declaration
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 4000;app.use(cors());
+const mongoose = require('mongoose');
+const PORT = 4000;
 // End const declaration
 
+app.use(cors());
+app.use(bodyParser.json());
+
+// Establish connection with DB 
+mongoose.connect('mongodb://127.0.0.1:27017', {
+    useNewUrlParser: true
+});
+const connection =  mongoose.connection;
+
 // Begin Logging
+connection.once('open', function(){
+    console.log("MongoDB database connection established successfully");
+})
 app.use(bodyParser.json());app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
