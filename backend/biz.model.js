f6d19af797e36fdcca1704b75c2e7f0b9049d8a2
Ordo-S <matt@spadarolabs.com>
2020-06-23 12:28:52 -0700

    Super basic Schema for DB in place

---
+++ backend/biz.model.js
@@ -0,0 +1,25 @@
+// Business Model Scheme 
+
+// Set up connection to mongo with mongoose
+const mongoose = require('mongoose');
+const Schema =  mongoose.Schema;
+
+let biz = new Schema({
+    biz_Name: {
+        type: String
+    },
+    biz_description: {
+        type: String
+    },
+    biz_address: {
+        type: String
+        // I would like this to be a multi-value field
+        // For now just a string
+    },
+    biz_type: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.model('biz',biz);
+
